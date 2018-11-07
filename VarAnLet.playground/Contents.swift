61f440f5bf14508ed233cf4428c2f22894c39c3b
supot10 <supot10@hotmail.com>
2018-11-07 10:21:55 +0700

    varAnlet

---
+++ VarAnLet.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//การประกาศตัวแปร
+
+//Implicit คือการประกาศ DataType จาก Value
+var nameString = "Doramon"
+var number1Int = 123
+var number2Dou = 12.34
+var status1Bool = true
+
+//Explicit คือการประกาศที่กำหนด DataType มาเลย
+var name2String: String = "MasterUNG"
+var number2Int: Int = 234
+var number3Dou: Double = 23.45
+var status2Bool: Bool = false
+
+//Constant การประกาศค่าคงที่
+let surnameString: String = "supot"
+
+name2String = "MasterUNG1234"
+
